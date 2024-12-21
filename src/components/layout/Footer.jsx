ea6214acbceeb2deaef6639b416544aa59b051c7
taknarae <dddddum@naver.com>
2024-12-22 03:46:16 +0900

    footer 수정

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -15,23 +15,23 @@ const Footer = () => {
       </div>
       <div className="fnb">
         <div className="layout_fix">
-          <h1 className="starbucks_logo"><Link to="/">스타벅스 로고</Link></h1>
-          <div className="info">
-            <div className="info_biz">
-              <span>회사명 : Starbucks Coffee Korea Co., Ltd.</span>
-              <span>사업장주소 : 서울특별시 중구 퇴계로 100, 8~10층(회현동2가)</span>
-              <span>대표 : 손정현</span>
-              <span>사업자등록번호 : 201-81-21515 <button>[사업자번호조회]</button></span>
-              <span>대표번호 : 1522-3232</span>
-              <span>개인정보 보호책임자 : 이찬우</span>
-            </div>
-            <div className="info_sns">
+          <div className="logo_sns">
+            <h1 className="starbucks_logo"><Link to="/">스타벅스 로고</Link></h1>
+            <div className="sns">
               <Link to="/" className="insta" title="인스타그램">인스타그램</Link>
               <Link to="/" className="youtube" title="유튜브">유튜브</Link>
               <Link to="/" className="x" title="트위터">트위터</Link>
               <Link to="/" className="facebook" title="페이스북">페이스북</Link>
               <Link to="/" className="blog" title="네이버 블로그">블로그</Link>
             </div>
+          </div>
+          <div className="biz">
+            <span>회사명 : Starbucks Coffee Korea Co., Ltd.</span>
+            <span>사업장주소 : 서울특별시 중구 퇴계로 100, 8~10층(회현동2가)</span>
+            <span>대표 : 손정현</span>
+            <span>사업자등록번호 : 201-81-21515 <button>[사업자번호조회]</button></span>
+            <span>대표번호 : 1522-3232</span>
+            <span>개인정보 보호책임자 : 이찬우</span>
           </div>
           <div className="copyright">&copy; 2024 Starbucks Coffee Company. All Rights Reserved.</div>
           <div className="link">
