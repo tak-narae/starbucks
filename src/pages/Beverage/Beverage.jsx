52528044bf9401a6d73cc928d92bed60eb8dde38
deliondane <hynnk0@gmail.com>
2024-12-26 20:16:22 +0900

    menu title 설정

--- src/pages/Beverage/Beverage.jsx
+++ src/pages/Beverage/Beverage.jsx
@@ -1,47 +1,46 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { DataContext } from "App";
 
 import "App.css";
 import "../guide/guide.css";
 import "../Main.css";
+import PrdList from "components/product/PrdList";
+
 
 const Beverage = () => {
-  const [category, setCategory] = useState([]);
-  const [categoryList, setCategoryList] = useState([]);
+  const { beverage, setBeverage } = useContext(DataContext); 
+  console.log(beverage);
+  // const [category, setCategory] = useState([]);
+  // const [categoryList, setCategoryList] = useState([]);
 
-  const getdata = async () => {
-    try {
-      const response = await axios.get(`http://localhost:8000/beverage`);
-      response.data.map((cate) => {
-        setCategory(prevCate => [...prevCate,cate.category]);
-        // console.log(setCategory);
-        setCategoryList(cate.products);
-        console.log(cate.products);
-      });
-    } catch (err) {
-      console.error("Error:", err);
-    }
-  };
-  useEffect(() => {
-    getdata();
-  }, []);
+  // const getdata = async () => {
+  //   try {
+  //     const response = await axios.get(`http://localhost:8000/beverage`);
+  //     response.data.map((cate) => {
+  //       setCategory(prevCate => [...prevCate,cate.category]);
+  //       // console.log(setCategory);
+  //       setCategoryList(cate.products);
+  //       console.log(cate.products);
+  //     });
+  //   } catch (err) {
+  //     console.error("Error:", err);
+  //   }
+  // };
+  // useEffect(() => {
+  //   getdata();
+  // }, []);
 
   return (
     <>
-      <div id="container" className="layout_fix">
-        <div className="heading">
-          <span className="sub">음료</span>
-          <h2 className="tit-em">음료</h2>
-        </div>
-      </div>
       <div className="category">
         <ul>
-            {
+            {/* {
               category.map((el, idx)=>(
                 <li key={idx}><Link to={`/beverage/${idx}`}>{el}</Link></li>
               ))
-            }
+            } */}
         </ul>
       </div>
       <div className="guideItem layout_fix">
@@ -55,7 +54,7 @@ const Beverage = () => {
                 </Link>
               </div>
               <div className="desc">
-                <div className="name">상품이름: {categoryList.name}</div>
+                <div className="name">상품이름</div>
                 <div className="price">39,000원</div>
                 <div className="review">4.9</div>
               </div>
