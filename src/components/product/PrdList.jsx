597f508b5c0432b6de6374a95ddfcdc4dafe28df
taknarae <dddddum@naver.com>
2025-01-02 21:52:38 +0900

    img 교체중

--- src/components/product/PrdList.jsx
+++ src/components/product/PrdList.jsx
@@ -11,7 +11,7 @@ import { Link } from "react-router-dom";
     prdSeason : 홀리데이정보(메인)
   === */ 
 
-const PrdList = ( {title, cateKo, pathName, selectedCate, selectedDepth, currentData, prdSeason, search} ) => {
+const PrdList = ( {title, cateKo, pathName, selectedCate, selectedDepth, currentData, prdSeason=[], search} ) => {
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
@@ -74,16 +74,23 @@ const PrdList = ( {title, cateKo, pathName, selectedCate, selectedDepth, current
           )
         ) : (
           // selectedCate나 selectedDepth가 없을 때 "홀리데이" 제품 목록 렌더링
-          prdSeason.map((el) => (
-            <li key={el.key}>
+          prdSeason.map((product) => (
+            <li key={product.key}>
               <div className="item">
-                <Link to={`/menu/${pathName}?cate=${selectedCate}&id=${el.id}`} className="thumbnail">
+                {/* <Link to={`/menu/${pathName}?cate=${selectedCate}&id=${el.id}`} className="thumbnail"> */}
+                <Link className="thumbnail"
+                  to={{
+                    pathname : `/menu/detail/${pathName}`,
+                    search: `?cate=${selectedCate}&id=${product.id}`,
+                  }}
+                  state={{ title, cateKo, product }}
+                  >
                   <div className="image">
-                      <img src={`${process.env.PUBLIC_URL}/${el.img}`} alt={el.name} />
+                      <img src={`${process.env.PUBLIC_URL}/${product.img}`} alt={product.name} />
                   </div>
                 </Link>
                 <div className="desc">
-                  <div className="name">{el.name}</div>
+                  <div className="name">{product.name}</div>
                   {/* <div className="review">4.9</div> */}
                   <div className="price">39,000원</div>
                 </div>
--- src/pages/Notice/Notice.jsx
+++ src/pages/Notice/Notice.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from "react";
-import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { DataContext } from "App";
 
