e5ee38ecb7b88a7f784e0df5794f0326dacb2270
deliondane <hynnk0@gmail.com>
2025-01-10 17:52:01 +0900

    main notice event 연결

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,55 +1,52 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { DataContext } from "App";
-import useQueryParams from 'hooks/useQueryParams';
+import useQueryParams from "hooks/useQueryParams";
 
 // swiper
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // css/js
 import "./Main.scss";
 
 // components
-import SplitEffect from './MainSplit';
-import PrdList from 'components/product/PrdList';
+import SplitEffect from "./MainSplit";
+import PrdList from "components/product/PrdList";
 import "components/product/PrdList.css";
 
-
 const Main = () => {
-  const { product } = useContext(DataContext);
+  const { product, notice, events } = useContext(DataContext);
   const { selectedCate, selectedDepth, pathName } = useQueryParams();
 
   const [prdSeason, setPrdSeason] = useState([]); // 홀리데이 상품
   useEffect(() => {
     const seasonAll = product.flatMap((el) => {
-      const seasonCate = el.products.filter((prd) => prd.name.includes("홀리데이"));
+      const seasonCate = el.products.filter((prd) =>
+        prd.name.includes("홀리데이")
+      );
       return seasonCate;
     });
     setPrdSeason(seasonAll); // "홀리데이" 제품만 추출
   }, [product]);
 
-  useEffect(()=>{
+  useEffect(() => {
     SplitEffect(); //.split
-  },[])
+  }, []);
 
   return (
     <>
       <main id="main">
         <section className="main__visual">
           <div className="layout_fix">
-            <div className="swiper-visual">
-              swiper-visual
-            </div>
+            <div className="swiper-visual">swiper-visual</div>
           </div>
           <div className="split">STARBUCKS</div>
         </section>
-        <section className="main__banner">
-          banner-img
-        </section>
+        <section className="main__banner">banner-img</section>
         <section className="main__prd">
           <div className="layout_fix">
             <div className="heading">
@@ -64,7 +61,9 @@ const Main = () => {
               prdSeason={prdSeason} // 시즌 상품 (홀리데이 제품) 전달
             />
             {/* selectedCate 확인하기 */}
-            <Link to="/menu/product?cate=0" className="btn_link">MORE</Link>
+            <Link to="/menu/product?cate=0" className="btn_link">
+              MORE
+            </Link>
           </div>
         </section>
         <section className="main__res_mz">
@@ -72,7 +71,9 @@ const Main = () => {
             <div className="heading">
               <span className="sub">Reserve Magazine</span>
               <h2 className="tit">리저브 매거진</h2>
-              <p className="desc-light">다채로운, 그리고 향기로운 커피 이야기</p>
+              <p className="desc-light">
+                다채로운, 그리고 향기로운 커피 이야기
+              </p>
             </div>
           </div>
         </section>
@@ -82,7 +83,8 @@ const Main = () => {
               <span className="sub-light">Starbucks</span>
               <h2 className="tit">리저브 매장</h2>
               <p className="desc-light">
-                내 취향이 머무는 곳,<br />
+                내 취향이 머무는 곳,
+                <br />
                 취향을 채우는 STARBUCKS RESERVE™ 매장
               </p>
             </div>
@@ -95,7 +97,8 @@ const Main = () => {
             </div>
             <div className="loop_event">
               <ul className="event_list">
-                <Swiper className="swiper_event"
+                <Swiper
+                  className="swiper_event"
                   modules={[Autoplay]}
                   loop={true}
                   slidesPerView="auto"
@@ -109,20 +112,25 @@ const Main = () => {
                     disableOnInteraction: false,
                   }}
                 >
+                  {events.slice(0,10).map((event, idx)=>(
                   <SwiperSlide>
-                    <Link to="/">
+                    <Link to="/event">
                       <div className="thumb">
-                        <img src={require("../images/bear_search1.png")} alt=""/>
+                        <img
+                          src={event.img}
+                          alt={event.title}
+                          style={{width: "100%", borderRadius: "8px"}}
+                        />
                       </div>
                       <div className="info">
-                        <p className="tit">조선델리 노엘 트리 케이크 사전 예약 혜택</p>
-                        <span className="date">2024-11-27~ 2024-12-25</span>
+                        <p className="tit">
+                          {event.title}
+                        </p>
+                        <span className="date">{event.startDate} ~ {event.endDate}</span>
                       </div>
                     </Link>
                   </SwiperSlide>
-                  <SwiperSlide><Link to="/">2</Link></SwiperSlide>
-                  <SwiperSlide><Link to="/">3</Link></SwiperSlide>
-                  <SwiperSlide><Link to="/">4</Link></SwiperSlide>
+                  ))}
                 </Swiper>
               </ul>
             </div>
@@ -131,41 +139,49 @@ const Main = () => {
         <section className="main__promo">
           <div className="layout_fix">
             <ul className="banner">
-              <li><Link to="/"><img src={require("../images/main_promo_banner1.png")}/></Link></li>
-              <li><Link to="/"><img src={require("../images/main_promo_banner2.png")}/></Link></li>
+              <li>
+                <Link to="/">
+                  <img src={require("../images/main_promo_banner1.png")} />
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <img src={require("../images/main_promo_banner2.png")} />
+                </Link>
+              </li>
             </ul>
             <div className="notice">
               <b>공지</b>
-              <Swiper className="swiper_notice"
-                  modules={[Autoplay, Navigation, Pagination]}
-                  slidesPerView={1}
-                  touchRatio={0}
-                  direction={"vertical"}
-                  autoHeight={true}
-                  autoplay={{
-                    delay: 2000,
-                    disableOnInteraction: false,
-                  }}>
-                    <SwiperSlide>
-                      <Link to="/">
-                        <p className="tit">11111-스마트로 PG(전자결제대행사)를 통한 일부 자동 충전 서비스 종료 사전 안내를 통한 일부 자동 충전 서비스 종료 사전 안내를 통한 일부 자동</p>
-                        <span className="date">2024-11-21</span>
+              <Swiper
+                className="swiper_notice"
+                modules={[Autoplay, Navigation, Pagination]}
+                slidesPerView={1}
+                touchRatio={0}
+                direction={"vertical"}
+                autoHeight={true}
+                autoplay={{
+                  delay: 2000,
+                  disableOnInteraction: false,
+                }}
+              >
+                {
+                notice && notice.length > 0 ? (
+                  notice.slice(0, 10).map((noticeItem, idx) => (
+                    <SwiperSlide key={idx}>
+                      <Link to="/notice">
+                        <p className="tit">{noticeItem.subject}</p>
+                        <span className="date">{noticeItem.date}</span>
                       </Link>
                     </SwiperSlide>
-                    <SwiperSlide>
-                      <Link to="/">
-                        <p className="tit">22222-스마트로 PG(전자결제대행사)를 통한 일부 자동 충전 서비스 종료 사전 안내</p>
-                        <span className="date">2024-11-18</span>
-                      </Link>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                      <Link to="/">
-                        <p className="tit">33333-스마트로 PG(전자결제대행사)를 통한 일부 자동 충전 서비스 종료 사전 안내</p>
-                        <span className="date">2024-11-14</span>
-                      </Link>
-                    </SwiperSlide>
+                  ))
+                ) : (
+                  <p>공지사항이 없습니다.</p>
+                )
+              }
               </Swiper>
-              <Link to="/" className="btn_link">더보기 +</Link>
+              <Link to="/notice" className="btn_link">
+                더보기 +
+              </Link>
             </div>
           </div>
         </section>
