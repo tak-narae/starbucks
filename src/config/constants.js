5bab06d3132809f02553d18e2d98432350992a4e
deliondane <hynnk@gmail.com>
2025-01-23 13:09:54 +0900

    API_URL server 수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL = process.env.NODE_ENV === "production" ? "https://port-0-starbucks-server-m68t82tub8d34ceb.sel4.cloudtype.app" : "http://localhost:8080";
